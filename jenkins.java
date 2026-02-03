82e478200a617996b91bd639e9598c8970d150c1
Pranav Harad <155727896+Pranav-Harad@users.noreply.github.com>
2026-02-03 09:39:02 +0530

    Add files via upload

---
+++ jenkins.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] a = s1.toLowerCase().toCharArray();
+        char[] b = s2.toLowerCase().toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("hello", "world"));   // false
+    }
+}
